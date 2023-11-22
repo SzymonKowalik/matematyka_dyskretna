5875eee8f0731a44558d86904d2dbb2eb5c755a8
Szymon Kowalik <szymon.kowalik44@gmail.com>
2023-11-23 00:12:40 +0100

    Unsuccessful test

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import itertools
+import numpy as np
+
 def tablica_prawdy(ile_zmiennych, czy_zanegowane, duplikaty_index):
 	wyn = []
 	for i in range((2**ile_zmiennych)):
+		wartosciowanie = itertools.product([0, 1], repeat=ile_zmiennych)
 		wartosciowanie = [int(x) for x in ((ile_zmiennych-1)*'0'+format(i, 'b'))[-ile_zmiennych:]]
+
+		print(wartosciowanie)
 		if czy_zanegowane:
 			wartosciowanie[0] = not wartosciowanie[0]
-
+		print(wartosciowanie)
+		print('-----')
 		xor = 0
 
 		for i, y in zip(wartosciowanie, duplikaty_index):
@@ -14,42 +21,31 @@ def tablica_prawdy(ile_zmiennych, czy_zanegowane, duplikaty_index):
 	return wyn
 
 
-def porownaj(l1, l2, max_zmienionych):
-	counter = 0
-	for il1, il2 in zip(l1, l2):
-		if il1 != il2:
-			if counter >= max_zmienionych:
-				return False
-			counter += 1
-	return True
+def porownaj(mozliwosc, znieksztalcona, max_zmienionych):
+	czy_niezmienione = np.array(mozliwosc) == np.array(znieksztalcona)
+	return np.count_nonzero(czy_niezmienione==0) <= max_zmienionych
 
 
 def mozliwosci(ile_zmiennych, przyklad, max_zmienionych):
-	count = 0
 	# mozliwosci
-	for x in range(2**ile_zmiennych):
-		count += 1
-		curr = ('0' * ile_zmiennych + format(x, 'b'))[-ile_zmiennych:]
-		curr = [int(x) for x in curr]
+	mozliwosci = itertools.product([0, 1], repeat=ile_zmiennych)
+	print(list(mozliwosci))
 
+	for x in mozliwosci:
 		# Bez negacji
-		mozliwy_szyfr = tablica_prawdy(ile_zmiennych, 0, curr)
+		mozliwy_szyfr = tablica_prawdy(ile_zmiennych, 0, x)
 		if porownaj(mozliwy_szyfr, przyklad, max_zmienionych):
-			return [0]+curr
+			return f"0{x}"
 		# Z negacją
-		mozliwy_szyfr = tablica_prawdy(ile_zmiennych, 1, curr)
+		mozliwy_szyfr = tablica_prawdy(ile_zmiennych, 1, x)
 		if porownaj(mozliwy_szyfr, przyklad, max_zmienionych):
-			return [1]+curr
+			return f"1{x}"
 
 
-def odkoduj(dlugosc_jednej_wiadomosci, ile_wiadomosci,  max_zmienionych_bitow, przyklady):
-	ile_zmiennych = dlugosc_jednej_wiadomosci - 1
-
-	i = 0
-	while i < ile_wiadomosci:
-		przyklad = [int(x) for x in przyklady[i]]
-		print(f"{mozliwosci(ile_zmiennych, przyklad, max_zmienionych_bitow)}")
-		i += 1
+def odkoduj(dlugosc_jednej_wiadomosci, 	max_zmienionych_bitow, przyklady):
+	for zakodowana in przyklady:
+		zakodowana = np.array([int(x) for x in zakodowana])
+		print(f"{mozliwosci(dlugosc_jednej_wiadomosci - 1, zakodowana, max_zmienionych_bitow)}")
 
 
 def uruchom(sciezka):
@@ -61,14 +57,21 @@ def uruchom(sciezka):
 	ile_przykladow = int(t[1])
 	max_zmienionych_bitow = int(t[2])
 
-	odkoduj(dlugosc_jednej_wiadomosci=dlugosc_wiadomosci, ile_wiadomosci=ile_przykladow,
+	odkoduj(dlugosc_jednej_wiadomosci=dlugosc_wiadomosci,
 			max_zmienionych_bitow=max_zmienionych_bitow, przyklady=przyklady)
 
 
+# def prawidlowa_mozliwosc(zmieniony, max_zmienionych):
+# 	mozliwosci = itertools.product([0, 1], repeat=len(zmieniony))
+# 	for mozliwosc in mozliwosci:
+# 		if porownaj(mozliwosc, zmieniony, max_zmienionych):
+# 			return mozliwosc
+
+
 if __name__ == '__main__':
-	path1 = "messages7.in.txt"
-	path2 = "messages13.in.txt"
-	path3 = "messages16.in.txt"
-	uruchom(path1)
-	uruchom(path2)
-	uruchom(path3)
+	# uruchom("messages7.in.txt")
+	# uruchom("messages13.in.txt")
+	# uruchom("messages16.in.txt")
+	# odkoduj(4, 1, ['01111001'])
+
+	print(prawidlowa_mozliwosc([0, 1, 1, 0, 1, 1, 0, 1], 1))
